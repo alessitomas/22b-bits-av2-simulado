dbf4c8b7944131201fe3b1bd00a4ac63f84a59aa
Alessi181 <tomasalessi@gmail.com>
2022-09-28 12:57:33 -0300

    updates

--- componentes.py
+++ componentes.py
@@ -6,7 +6,7 @@
 
 
 from myhdl import *
-from .ula_aux import *
+from ula_aux import *
 
 
 def exe1(a, b, c, q):
@@ -37,8 +37,39 @@ def exe3(x1, x0, y1, y0, z3, z2, z1, z0):
 
 
 def exe4_ula(a, b, inverte_a, inverte_b, c_in, c_out, selecao, zero, resultado):
+    aneg = Signal(modbv(0)[32:])
+    bneg = Signal(modbv(0)[32:])
+    adderout = Signal(modbv(0)[32:])
+    muxa_out= Signal(modbv(0)[32:])             
+    muxb_out = Signal(modbv(0)[32:]) 
+    orout, andout,slt, muxbigout = [Signal(modbv(0)[32:]) for i in range(4)]
+    muxa = mux2way(muxa_out, a, aneg,inverte_a)
+    muxb = mux2way(muxb_out, b, bneg,inverte_b)
+    adder1 = adder(adderout,c_out,muxa_out,muxb_out,c_in)
+    
+    
+    muxbig = mux4way(muxbigout,andout,orout,adderout,slt,selecao)
+
+    
+
+    
+    
+
+
+
     @always_comb
     def comb():
-        resultado.next = 0
+        andout.next = muxa_out & muxb_out
+        orout.next = muxa_out | muxb_out
+        aneg.next = not a
+        bneg.next = not b
+        if muxbigout == 0:
+            zero.next = 1
+        else:
+            zero.next = 0
+        resultado.next = muxbigout
 
+
+
+    
     return instances()
